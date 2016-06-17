b098b623d7d992a7ae8ce6c5d8bd0a347cdf2866
Jeffrey Dick <Jeffrey.J.Dick@ehi.com>
2016-06-17 16:26:46 -0500

    AppTheme

--- Test/app/src/androidTest/java/com/test/MainActivityTest.java
+++ Test/app/src/androidTest/java/com/test/MainActivityTest.java
@@ -3,11 +3,19 @@ package com.test;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.test.ActivityUnitTestCase;
+import android.view.ContextThemeWrapper;
 
 public class MainActivityTest extends ActivityUnitTestCase<MainActivity>{
 
     public MainActivityTest() {
         super(MainActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+        ContextThemeWrapper context = new ContextThemeWrapper(getInstrumentation().getTargetContext(), R.style.AppTheme);
+        setActivityContext(context);
     }
 
     public void testMainActivity() throws Exception {
